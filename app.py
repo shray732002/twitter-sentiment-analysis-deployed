be28ee0ad5c7509f5a5e45b41c4752129d6a02cf
Shray Kumar Singh <96106431+shray732002@users.noreply.github.com>
2022-07-09 18:32:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+from flask import Flask,request,render_template
+ps = PorterStemmer()
+import re
+
+app = Flask(__name__)
+def remove(text):
+    r = re.findall("@[\w]*",text)
+    for i in r:
+        tweet = re.sub(i, '', text)
+    return tweet 
+def transform(text):
+    text = text.lower()
+    words = nltk.word_tokenize(text)
+    y= []
+    for i in words:
+        i = ps.stem(i)
+        y.append(i)
+    return " ".join(y)
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('sentiment_analysis.pkl','rb'))
+
+@app.route('/',methods=['GET','POST'])
+def predict_loan_status():
+    if request.method=='POST':
+        tweet = request.form['tweet'] 
+        new_tweet = remove(tweet)
+        y = []
+        for i in new_tweet:
+            if i.isalnum() or i=='#':
+                y.append(i)
+        tweet = ' '.join(y)        
+
+        transformed = transform(tweet)
+        vector_input = tfidf.transform([transformed]).toarray()
+        result = model.predict(vector_input)
+        if result == 0:
+           result = " not racist/sexist"
+        else:
+           result = "racist/sexist"
+        return render_template("index.html",predictions=str(result))
+    return render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug = True)       
