fa1c786adfc1cfd29508f790214449522a9e99d2
Shray Kumar Singh <96106431+shray732002@users.noreply.github.com>
2022-07-09 17:16:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
 from flask import Flask,request,render_template
 ps = PorterStemmer()
 import re
